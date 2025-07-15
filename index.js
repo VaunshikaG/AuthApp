1be38cfa4e543759b5c1931a2222e028137c8684
vaunshika <vaunshika.gogarkar@fundspi.com>
2025-07-15 12:48:00 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 import { name as appName } from './app.json';
+import { Router } from './src/routes/Router';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Router);
